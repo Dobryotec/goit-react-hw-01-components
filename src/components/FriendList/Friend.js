731f913bd994c76c430a31543102aeb80d1fcbbf
Dobrobatko Dmytro <dobrobatka@gmail.com>
2023-03-18 22:44:04 +0200

    add update

--- src/components/FriendList/Friend.js
+++ src/components/FriendList/Friend.js
@@ -8,18 +8,12 @@ const colorOffline = {
   color: 'red',
 };
 const Friend = ({ name, avatar, status }) => {
+  const colorStyle = status ? colorOnline : colorOffline;
   return (
     <li className={css.item}>
-      {status ? (
-        <span className={css.status} style={colorOnline}>
-          <CiCircleCheck />
-        </span>
-      ) : (
-        <span className={css.status} style={colorOffline}>
-          <CiCircleCheck />
-        </span>
-      )}
-
+      <span className={css.status} style={colorStyle}>
+        <CiCircleCheck />
+      </span>
       <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
       <p>{name}</p>
     </li>
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 const Statistics = ({ title, stats }) => {
   return (
     <section className={css.section}>
-      {title && <h2 className={css.title}>Upload stats</h2>}
+      {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css['stat-list']}>
         {stats.map(({ id, label, percentage }) => {
           return <Statistic key={id} label={label} percentage={percentage} />;
